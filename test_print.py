8e7e7e90d0bc4fd9969c2ef82b35d85d4d2511f3
Aditi <65910955+Aditi15091999@users.noreply.github.com>
2022-09-10 14:45:56 +0530

    Update test_print.py

--- test_print.py
+++ test_print.py
@@ -3,4 +3,7 @@ from Hello import *
 
 class Test_print(unittest.TestCase):
   def test_print(self):
-    self.assertEqual(print(),"Hello aditi!!")
+    self.assertEqual(name(),"Hello aditi!!")
+    
+if __name__ == '__main__':
+    unittest.main()
